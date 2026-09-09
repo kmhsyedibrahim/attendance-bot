31b533b9b0276b3dcf5642ca87530add7e65b42d
kmhsyedibrahim <kmhsyedibrahim@gmail.com>
2026-09-09 20:39:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const EMPLOYEES = {
   '918778274487': { name: 'Rasheed', tab: 'RAS' },
   '917010171009': { name: 'Jaffer', tab: 'JAF' },
   '919042084992': { name: 'Harris', tab: 'HAR' },
-  '918300635880': { name: 'KSI', tab: 'KSI' },
+  '919894572851': { name: 'Soofi', tab: 'KSI' },
 };
 
 const COLUMN_MAP = {
