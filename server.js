a16711188830520f0e8dbf2361d415ec0ff13789
kmhsyedibrahim <kmhsyedibrahim@gmail.com>
2026-09-26 13:11:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const sheets = google.sheets({ version: 'v4', auth });
 
 const EMPLOYEES = {
   '918778274487': { name: 'Rasheed', tab: 'RAS' },
-  '917010171009': { name: 'Jaffer', tab: 'JAF' },
+  '917397504192': { name: 'Jaffer', tab: 'JAF' },
   '919042084992': { name: 'Harris', tab: 'HAR' },
   '918300635880': { name: 'KSI', tab: 'KSI' },
 };
